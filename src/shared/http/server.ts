5221db34f1c75db31ffa86293b03cad993980f5c
André Pizzolatti <andrepizzolatti@gmail.com>
2024-07-12 13:57:31 -0300

    validação de requests em /users via celebrate

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -5,6 +5,7 @@ import cors from 'cors';
 import routes from './routes';
 import errorHandler from '@middlewares/errorHandler';
 import '@shared/typeorm';
+import { errors } from 'celebrate';
 
 const app = express();
 
@@ -12,6 +13,8 @@ app.use(cors());
 app.use(express.json());
 
 app.use(routes);
+
+app.use(errors());
 
 app.use(errorHandler);
 
