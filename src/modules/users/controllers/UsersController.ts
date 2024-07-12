f408a49af9d195ffbb8b048e100c151fba4f9ec1
André Pizzolatti <andrepizzolatti@gmail.com>
2024-07-12 15:38:08 -0300

    Users controler

---
+++ src/modules/users/controllers/UsersController.ts
@@ -0,0 +1,57 @@
+import { Request, Response } from 'express';
+import ListUserService from '../services/ListUserService';
+import ShowUserService from '../services/ShowUserService';
+import CreateUserService from '../services/CreateUserService';
+import UpdateUserService from '../services/UpdateUserService';
+import DeleteUserService from '../services/DeleteUserService';
+
+export default class UsersController {
+  public async index(request: Request, response: Response): Promise<Response> {
+    const listUsers = new ListUserService();
+
+    const users = await listUsers.execute();
+
+    return response.json(users);
+  }
+
+  public async Show(request: Request, response: Response): Promise<Response> {
+    const { id } = request.params;
+
+    const showUser = new ShowUserService();
+
+    const user = await showUser.execute({ id });
+
+    return response.json(user);
+  }
+
+  public async create(request: Request, response: Response): Promise<Response> {
+    const { name, email, password } = request.body;
+
+    const createUser = new CreateUserService();
+
+    const user = await createUser.execute({ name, email, password });
+
+    return response.json(user);
+  }
+
+  public async update(request: Request, response: Response): Promise<Response> {
+    const { id } = request.params;
+    const { name, email, avatar } = request.body;
+
+    const updateUser = new UpdateUserService();
+
+    const user = await updateUser.execute({ id, name, email, avatar });
+
+    return response.json(user);
+  }
+
+  public async delete(request: Request, response: Response): Promise<Response> {
+    const { id } = request.params;
+
+    const deleteUser = new DeleteUserService();
+
+    await deleteUser.execute({ id });
+
+    return response.status(204).json();
+  }
+}
