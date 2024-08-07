97cc8b06906c2d6c4a86a359ccec7bd1c946bbee
André Pizzolatti <andrepizzolatti@gmail.com>
2024-08-07 12:51:29 -0300

    created orders and orders_products migrations

---
+++ src/shared/typeorm/migrations/1722899805486-CreateOrdersProducts.ts
@@ -0,0 +1,34 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+
+export class CreateOrdersProducts1722899805486 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'orders_products',
+        columns: [
+          {
+            name: 'id',
+            type: 'uuid',
+            isPrimary: true,
+            generationStrategy: 'uuid',
+            default: 'uuid_generate_v4()',
+          },
+          {
+            name: 'price',
+            type: 'decimal',
+            precision: 10,
+            scale: 2,
+          },
+          {
+            name: 'quantity',
+            type: 'int',
+          },
+        ],
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('orders_products');
+  }
+}
